40e7c8076a9c17f94d1d81ea3e35bf87b4621c46
hubinsord <hubinsord@gmail.com>
2021-11-09 00:34:24 +0100

    feature: provided "fresh" api access key

--- app/src/main/java/pl/qpony/currencyapp/core/di/Constants.kt
+++ app/src/main/java/pl/qpony/currencyapp/core/di/Constants.kt
@@ -2,6 +2,6 @@ package pl.qpony.currencyapp.core.di
 
 object Constants {
 
-    const val FIXER_API_ACCESS_KEY = "83fa6a48bd4d2a3ea82e1e0d858bcc01"
+    const val FIXER_API_ACCESS_KEY = "8289d77366dd941a521b46fc6d64c102"
     const val FIXER_BASE_URL = "http://data.fixer.io/api/"
 }
